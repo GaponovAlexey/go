030584a55c7b62002097e9682853dc1c11fa1302
Kekelberi <alexei.gaponov@gmail.com>
2022-06-26 16:43:39 +0300

    save

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
-	bob:= Cats {"Bob", 7, 0.92}
-	fmt.Println(bob.age);
+	bob := Cats{"Bob", 7, 0.92}
+	fmt.Println(bob.happiness)
+	fmt.Println(time.Now().Day())
+	fmt.Println(time.Now().Clock())
+	fmt.Println(rand.Intn(10))
+	
+
 }
+
 type Cats struct {
-	name string
-	age int
+	name      string
+	age       int
 	happiness float64
 }
