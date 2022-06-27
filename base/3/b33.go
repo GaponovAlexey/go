950678e1590e319e43553b312574f36b842a9fd3
Kekelberi <alexei.gaponov@gmail.com>
2022-06-27 19:28:35 +0300

    save

--- base/3/b33.go
+++ base/3/b33.go
@@ -3,6 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("start")
-
+	// append
+	s := []int{}
+	printSlice(s)
+	s = append(s, 1, 2, 5, 6)
+	printSlice(s)
+	s = s[1:3]
+	printSlice(s)
+	//range
+	for i, v:= range pow {
+		
+	}
 }
+var pow = []int{1,2,3,4,55, 66, 77}
+//append
+func printSlice(s []int) {
+	fmt.Printf("len%d cap=%d %v\n", len(s), cap(s), s)
+}
+//range
