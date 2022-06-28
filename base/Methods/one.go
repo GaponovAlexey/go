d60f2372481cfad4f7e7490e2fbe7a944fb3d4a0
kekelberi <alexei.gaponov@gmail.com>
2022-06-28 22:31:18 +0300

    save

--- base/Methods/one.go
+++ base/Methods/one.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 type Ver struct {
@@ -16,7 +17,7 @@ func (hera Ver) ass(s int) int {
 }
 
 // type MF float64
-type MF float64
+type MF int
 
 func (f MF) ADS() float64 {
 	if f < 0 {
@@ -25,11 +26,29 @@ func (f MF) ADS() float64 {
 	return float64(f)
 }
 
+type Vertex struct {
+	X, Y float64
+}
+
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
 func main() {
 	//methods
 	data := Ver{22, 23, 24, 25, 26}
 	fmt.Println(data.add())
 	fmt.Println(data.ass(22))
 	fmt.Println(data)
+	ff := MF(-2)
+	fmt.Println(ff.ADS())
+	v := Vertex{3, 4}
+	v.Scale(1)
+	fmt.Println(v.Abs())
 
 }
