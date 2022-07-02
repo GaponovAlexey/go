1dc3e51a36f3ab0a5f7c42d94086ba27575bda9c
kekelberi <alexei.gaponov@gmail.com>
2022-07-02 20:03:50 +0300

    save

--- base/Methods/2.go
+++ base/Methods/2.go
@@ -5,16 +5,20 @@ import (
 )
 
 func main() {
-	mes := make([]string, 5) // cap 5
-	mes = append(mes, "5")   // cap 10
-	mes = append(mes, "6")
-	mes = append(mes, "7")
-	mes = append(mes, "8")
-	mes = append(mes, "9")
-	mes = append(mes, "10") //cap 20
-	// cap() - Огранчение 10000 пл 10001
-	fmt.Println(len(mes))
-	fmt.Println(cap(mes))
-	fmt.Println(mes)
+	data := []string{
+		"mes_1",
+		"mes_2",
+		"mes_3",
+		"mes_4",
+	}
+	fmt.Println(data)
+	c := 0
+	for {
+		c++
+		fmt.Println(c)
+		if c == 100 {
+			break
+		}
+	}
 
 }
