2d53b044f05e9090652afe4f07b26dc5cee207f9
kekelberi <alexei.gaponov@gmail.com>
2022-07-03 18:12:05 +0300

    save

--- base/Methods/2.go
+++ base/Methods/2.go
@@ -4,15 +4,7 @@ import (
 	"fmt"
 )
 
-type Age int
-
 func main() {
-	// user1 := Tuser{"Vasia", 22, "male", 76, 122}
+	fmt.Println("my module")
 
-	fmt.Println(Age.isAge(22))
-
-}
-
-func (a Age) isAge() bool {
-	return a >= 20
 }
