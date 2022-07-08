e73a3a23f3ed35ad1a296eb0cd173cf1fea4d332
Kekelberi <alexei.gaponov@gmail.com>
2022-07-08 23:17:11 +0300

    wq

--- Itej/struct.go
+++ Itej/struct.go
@@ -4,9 +4,14 @@ import (
 	"fmt"
 )
 
-
+type Users struct {
+	name     string
+	age      int64
+	password string
+}
 
 func main() {
-	
+	user := Users{name: "Ji", age: 22, password: "23312"}
+	fmt.Println(user)
 	
 }
