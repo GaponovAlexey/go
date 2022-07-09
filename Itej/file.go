9ecf09816e7605d15b03a49de22116dd44117c46
Kekelberi <alexei.gaponov@gmail.com>
2022-07-09 19:09:41 +0300

    save

--- Itej/file.go
+++ Itej/file.go
@@ -3,14 +3,19 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 )
 
 func main() {
-	fmt.Println("file")
-	file_data, err := ioutil.ReadFile("text.txt")
+	// create
+	data:= []byte("text to file")
+	err := ioutil.WriteFile("Itej/text.txt", data, 0600 )
+// read
+	file_data, err := ioutil.ReadFile("Itej/text.txt")
 	if err != nil {
 		fmt.Println("ne mogu pro4itat")
 	}
 	fmt.Println(string(file_data))
 
+	os.Remove("Itej/text.txt")
 }
