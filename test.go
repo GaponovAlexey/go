77a6a9d088c5edf210b4290336f64b8a19422e40
kekelberi <alexei.gaponov@gmail.com>
2022-07-22 16:12:50 +0300

    save

--- test.go
+++ test.go
@@ -2,12 +2,28 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"time"
 )
 
 func main() {
-	v := true
-	s := strconv.FormatBool(v)
-	fmt.Printf("%T, %v\n", s, s)
+	ch := make(chan int)
+	// ch2 := make(chan int)
+	go rut1(ch)
 
+	for {
+		fmt.Println(<-ch)
+		break
+	}
+
+	time.Sleep(time.Second * 2)
+}
+
+func rut1(ch chan int) {
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+		
+		// time.Sleep(time.Microsecond)
+		ch <- i
+	}
+	defer close(ch)
 }
