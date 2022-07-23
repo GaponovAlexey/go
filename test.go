3562049b47a984e8d55c62782ddc672773b49653
kekelberi <alexei.gaponov@gmail.com>
2022-07-23 19:10:36 +0300

    xs

--- test.go
+++ test.go
@@ -1,29 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"time"
+import "fmt"
+
+const (
+	min int = 1
+	max int = 5
 )
 
 func main() {
-	ch := make(chan int)
-	// ch2 := make(chan int)
-	go rut1(ch)
+	fmt.Println("sad")
 
-	for {
-		fmt.Println(<-ch)
-		break
-	}
-
-	time.Sleep(time.Second * 2)
-}
-
-func rut1(ch chan int) {
-	for i := 0; i < 5; i++ {
-		fmt.Println(i)
-		
-		// time.Sleep(time.Microsecond)
-		ch <- i
-	}
-	defer close(ch)
 }
