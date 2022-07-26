ccc7535fd4be1381650f8b1f1f57bc92a077d103
kekelberi <alexei.gaponov@gmail.com>
2022-07-26 21:49:43 +0300

    proz

--- test.go
+++ test.go
@@ -1,35 +1,66 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"runtime"
+	"sync"
 	"time"
 )
 
 func main() {
-	t := time.Now()
-	buf := make(chan int)
+	workerPool()
+}
 
-	n := func() int {
-		i := 0
-		for ; i <= 100; i++ {
-			i++
-		}
-		return i
-	}()
+func workerPool() {
+	ctx, cancel := context.WithCancel(context.Background())
+	ctx, cancel = context.WithTimeout(context.Background(), time.Microsecond*20)
+	defer cancel()
 
-	fmt.Println("100:", n)
+	wg := &sync.WaitGroup{}
 
-	num := []int{1, 2, 3, 4, 5}
-	go func() {
-		for _, i := range num {
-			buf <- i
-		}
-		close(buf)
-	}()
+	tp, pns := make(chan int, 5), make(chan int, 5)
 
-	for v := range buf {
-		fmt.Println(v)
+	for i := 0; i <= runtime.NumCPU(); i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			worker(ctx, tp, pns)
+		}()
 	}
-	fmt.Println(time.Since(t).Nanoseconds())
 
+	go func() {
+		for i := 0; i <= 1000; i++ {
+			tp <- i
+		}
+		close(tp)
+	}()
+
+	go func() {
+		wg.Wait()
+		close(pns)
+	}()
+
+	var counter int
+	for res := range pns {
+		counter++
+		fmt.Println(res)
+	}
+	fmt.Println(counter)
+
+}
+
+func worker(ctx context.Context, toP <-chan int, proc chan<- int) {
+	for {
+		select {
+		case <-ctx.Done(): // дожидается завершения контекста
+			return
+		case v, ok := <-toP:
+			if !ok {
+				return
+			}
+			time.Sleep(time.Microsecond)
+			proc <- v * v
+		}
+	}
 }
