e34f8b37344297400501a114435f6c7e9644a25d
kekelberi <alexei.gaponov@gmail.com>
2022-07-27 22:38:03 +0300

    SAVE

--- main.go
+++ main.go
@@ -2,61 +2,17 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"sync/atomic"
-	"time"
 )
 
 func main() {
-	add()
-	addAtomic()
-
-}
-
-func add() {
-	s := time.Now()
-	var (
-		c  int64
-		wg sync.WaitGroup
-		m  sync.Mutex
-	)
-
-	for i := 0; i < 1000; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-
-			m.Lock()
-			c++
-			m.Unlock()
-		}()
-	}
-	wg.Wait()
-	fmt.Println(c)
-
-	fmt.Println(time.Since(s).Seconds())
-}
-
-func addAtomic() {
-	s := time.Now()
-	var (
-		c  int64
-		wg sync.WaitGroup
-	)
-	wg.Add(1000)
+	var c int64
 	
+	atomic.StoreInt64(&c, 6)
+	fmt.Println(atomic.LoadInt64(&c))
 	
-	for i := 0; i < 1000; i++ {
-		go func() {
-			defer wg.Done()
-			atomic.AddInt64(&c, 1)
-			atomic.AddInt64(&c, -1)
-			atomic.AddInt64(&c, 1)
-			}()
-		}
-		wg.Wait()
-		fmt.Println("Atomic:",atomic.LoadInt64(&c))
-	fmt.Println(c)
+	fmt.Println(atomic.SwapInt64(&c, 10))
+	fmt.Println(atomic.LoadInt64(&c))
+	
 
-	fmt.Println(time.Since(s).Seconds())
 }
