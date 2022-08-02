e8c0da743eb60e6ac87d825c6e96218c4969b3e1
kekelberi <alexei.gaponov@gmail.com>
2022-08-02 21:32:07 +0300

    save

--- main.go
+++ main.go
@@ -6,25 +6,6 @@ import (
 )
 
 func main() {
-data:= []int{1,2,3}
-fmt.Println(reflect.TypeOf(data))
+fmt.Println("da")
 
-
-	m := map[string]interface{}{}
-	m["one"] = 1
-	m["two"] = 2.0
-	m["three"] = true
-	for k, v := range m {
-		switch v.(type) {
-		case bool:
-			fmt.Printf("%v is an bool %v\n", k, v)
-		case int:
-			fmt.Printf("%v is an int %v\n", k, v)
-		case float64:
-			fmt.Printf("%v is an float64 %v\n", k, v)
-			default:
-			fmt.Printf("%v os %v\n", k, reflect.TypeOf(v))
-
-		}
-	}
 }
