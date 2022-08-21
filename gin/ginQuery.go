0a7dcc943cc02335f5f7cb542bc89117763ec342
kekelberi <alexei.gaponov@gmail.com>
2022-08-21 22:07:40 +0300

    save

--- gin/ginQuery.go
+++ gin/ginQuery.go
@@ -6,20 +6,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var nWeapon = map[string]string{
+var nWeapon = map[string]interface{}{
 	"ninja": "start",
 }
 
 func main() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		wType := c.Query("type")
-		wName, ok := nWeapon[wType]
-		if !ok {
-			c.JSON(404, gin.H{
-				"status": "404",
-			})
-		}
+	r.GET("/", func(c *gin.Context) { // 3000/?type=ninja
+		
+    wType := c.Query("type")
+
+		wName := nWeapon[wType]
+
 		c.JSON(http.StatusOK, gin.H{
 			wType: wName,
 		})
