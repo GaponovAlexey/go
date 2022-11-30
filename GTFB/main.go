0c0052b7da0c30f4e02c8e4e370a72fb0c330a8d
kekelberi <alexei.gaponov@gmail.com>
2022-11-29 22:56:42 -0600

    save

--- GTFB/main.go
+++ GTFB/main.go
@@ -1,3 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"log"
+)
 
+func main() {
+	var userName string
+	var books uint8
+
+	books = 3
+	books = 255
+	// fmt.Scan(&userName)
+	fmt.Scan(books, userName)
+	log.Println(&userName)
+
+}
