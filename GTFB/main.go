95e9535ac832787acc7879811257eace86161fdf
kekelberi <alexei.gaponov@gmail.com>
2022-12-01 23:20:21 -0600

    save

--- GTFB/main.go
+++ GTFB/main.go
@@ -1,26 +1,10 @@
 package main
 
-import (
-	"fmt"
-
-)
-
-type Vertex struct {
-	X, Y string
-}
+import "fmt"
 
 func main() {
-	b := make([]int, 5, 5) // len(b)=0, cap(b)=5
-	b = append(b, 10, 11)
-	b[0] = 10
-
-	fmt.Println("B = ", b, len(b), cap(b))
-
-	c := make([]int, len(b))
-	copy(c, b)
-
-	fmt.Println("\nC = ", c, len(c), cap(c))
-	fmt.Println(b)
-	fmt.Println(c)
-
+	i := 0
+	i = i << uint(2)
+	fmt.Println(i)
+	
 }
