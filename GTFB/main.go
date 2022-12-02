e614dddf39f82f72166f6021b007f3d65703bc94
kekelberi <alexei.gaponov@gmail.com>
2022-12-01 22:46:19 -0600

    save

--- GTFB/main.go
+++ GTFB/main.go
@@ -2,10 +2,25 @@ package main
 
 import (
 	"fmt"
-	"time"
 
 )
 
+type Vertex struct {
+	X, Y string
+}
+
 func main() {
-	fmt.Print(time.Now().Hour())
+	b := make([]int, 5, 5) // len(b)=0, cap(b)=5
+	b = append(b, 10, 11)
+	b[0] = 10
+
+	fmt.Println("B = ", b, len(b), cap(b))
+
+	c := make([]int, len(b))
+	copy(c, b)
+
+	fmt.Println("\nC = ", c, len(c), cap(c))
+	fmt.Println(b)
+	fmt.Println(c)
+
 }
